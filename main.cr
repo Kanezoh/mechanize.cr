d4d4cc97b3bb23117e533dbb8c43a4efd65e6f7d
Kanezoh <asfaltmanta114@gmail.com>
2021-05-04 09:56:02 +0900

    add parser

--- main.cr
+++ main.cr
@@ -4,5 +4,6 @@ agent = Mechanize.new
 agent.request_headers = HTTP::Headers{"Foo" => "Bar"}
 params = {"hoge" => "hoge"}
 page = agent.get("http://example.com/", params: params)
-puts page.code
-puts page.body
+#puts page.code
+#puts page.body
+puts page.parser.not_nil!.css("h1").first.inner_text
--- src/mechanize/page.cr
+++ src/mechanize/page.cr
@@ -1,6 +1,14 @@
+require "myhtml"
 require "./file"
+
 class MechanizeCr::Page < MechanizeCr::File
   def initialize(uri, response, body, code)
     super(uri, response, body, code)
+  end
+
+  def parser
+    return @parser if @parser
+    return unless @body
+    @parser = Myhtml::Parser.new(@body)
   end
 end
