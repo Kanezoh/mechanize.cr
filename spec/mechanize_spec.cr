c690b05d13f34a66a589429c71050ae24d912550
Kanezoh <asfaltmanta114@gmail.com>
2021-06-08 21:20:46 +0900

    add webmock

--- spec/mechanize_spec.cr
+++ spec/mechanize_spec.cr
@@ -1,9 +1,13 @@
 require "./spec_helper"
+require "webmock"
+WebMock.stub(:any, "example.com")
 
 describe Mechanize do
   # TODO: Write tests
 
   it "works" do
-    false.should eq(true)
+    response = HTTP::Client.get("http://example.com")
+    response.body.should eq ""
+    response.status_code.should eq 200
   end
 end
