88f973d2afa101a034cd52838d9c7b0e3760b499
Kanezoh <asfaltmanta114@gmail.com>
2021-06-10 17:09:36 +0900

    add http test

--- spec/mechanize_spec.cr
+++ spec/mechanize_spec.cr
@@ -1,6 +1,7 @@
 require "./spec_helper"
 require "webmock"
-WebMock.stub(:any, "example.com")
+WebMock.stub(:get, "example.com")
+WebMock.stub(:get, "http://example.com/?foo=bar&foo1=bar2")
 
 describe Mechanize do
   it "simple GET" do
@@ -12,12 +13,29 @@ describe Mechanize do
   end
 
   it "GET with query parameter" do
-    WebMock.stub(:get, "http://example.com/?foo=bar&foo1=bar2")
     agent = Mechanize.new
     params = {"foo" => "bar", "foo1" => "bar2"}
     uri = "http://example.com/"
     page = agent.get(uri, params: params)
     page.code.should eq 200
     page.uri.to_s.should eq "http://example.com/?foo=bar&foo1=bar2"
+  end
+
+  it "GET with query parameter as URL string" do
+    agent = Mechanize.new
+    uri = "http://example.com/?foo=bar&foo1=bar2"
+    page = agent.get(uri)
+    page.code.should eq 200
+    page.uri.to_s.should eq uri
+  end
+
+  it "set custom request headers" do
+    agent = Mechanize.new
+    uri = "http://example.com/"
+    headers = HTTP::Headers{"Foo" => "Bar"}
+    agent.request_headers.empty?.should eq true
+    page = agent.get(uri, headers: headers)
+    agent.request_headers.empty?.should eq false
+    agent.request_headers["Foo"].should eq "Bar"
   end
 end
