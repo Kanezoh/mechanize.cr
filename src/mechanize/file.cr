fe15e4a0492c555a523f2ecb7c2e95af396a7c68
Kanezoh <asfaltmanta114@gmail.com>
2021-06-10 17:10:48 +0900

    delete unneccessary nil definition

--- src/mechanize/file.cr
+++ src/mechanize/file.cr
@@ -1,8 +1,8 @@
 require "http/client"
 class MechanizeCr::File
   #property :body, :filename
-  property :body, :code, uri
-  def initialize(uri : URI, response : ::HTTP::Client::Response | Nil, body : String , code : Int32 | Nil)
+  property :body, :code, uri, :response
+  def initialize(uri : URI, response : ::HTTP::Client::Response, body : String , code : Int32)
     @uri  = uri
     @body = body
     @code = code
