1f97287eaf7f983611483b704e47007f2ddbc3e4
Kanezoh <asfaltmanta114@gmail.com>
2021-08-09 17:49:06 +0900

    add uri escape test

--- spec/http_spec.cr
+++ spec/http_spec.cr
@@ -3,6 +3,7 @@ WebMock.stub(:get, "http://example.com/?foo=bar&foo1=bar2")
 WebMock.stub(:post, "http://example.com/post").
          with(body: "email=foobar", headers: {"Content-Type" => "application/x-www-form-urlencoded"}).
          to_return(body: "success")
+WebMock.stub(:get, "example.com/%E3%81%82%E3%81%82%E3%81%82")
 
 describe "Mechanize HTTP test" do
   it "simple GET" do
@@ -28,6 +29,12 @@ describe "Mechanize HTTP test" do
     page = agent.get(uri)
     page.code.should eq 200
     page.uri.to_s.should eq uri
+  end
+
+  it "can escape non-ascii character" do
+    agent = Mechanize.new
+    page = agent.get("http://example.com/あああ")
+    page.uri.to_s.should eq "http://example.com/%E3%81%82%E3%81%82%E3%81%82"
   end
 
   it "set custom request headers" do
