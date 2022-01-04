712147aa701d9428f8c280507f86f8f8eca84c71
Kanezoh <asfaltmanta114@gmail.com>
2022-01-04 09:53:04 +0900

    prepare basic auth test

--- spec/http_auth_spec.cr
+++ spec/http_auth_spec.cr
@@ -1,15 +1,18 @@
 require "./spec_helper"
-
-WebMock.stub(:get, "http://auth.example.com/")
-  .with(headers: {"Authorization" => "Basic #{Base64.strict_encode("user:pass").chomp}"})
-  .to_return(status: 200)
-
-WebMock.stub(:get, "http://auth.example.com/")
-  .to_return(status: 401, headers: {"WWW-Authenticate" => "Basic realm=\"Access to the staging site\", charset=\"UTF-8\""})
+require "./server.cr"
+#WebMock.stub(:get, "http://auth.example.com/")
+#  .with(headers: {"Authorization" => "Basic #{Base64.strict_encode("user:pass").chomp}"})
+#  .to_return(status: 200)
+#
+#WebMock.stub(:get, "http://auth.example.com/")
+#  .to_return(status: 401, headers: {"WWW-Authenticate" => "Basic realm=\"Access to the staging site\", charset=\"UTF-8\""})
 
 describe "Mechanize HTTP Authentication test" do
-  # it "auth" do
-  #  agent = Mechanize.new
-  #  agent.get("http://auth.example.com/")
-  # end
+   WebMock.allow_net_connect = true
+   it "auth" do
+    agent = Mechanize.new
+    page = agent.get("#{TEST_SERVER_URL}/secret")
+    p page.body
+    WebMock.allow_net_connect = false
+   end
 end
